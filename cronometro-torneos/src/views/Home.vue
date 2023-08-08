93ce19aa3d9013e253b2c8a49a422d4aa51fcbb5
orlandobel <orlando.bel.jimmy@gmail.com>
2023-08-08 00:11:09 -0600

    Project created

---
+++ cronometro-torneos/src/views/Home.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import CompetitionButton from "../components/Buttons/CompetitionButton.vue";
+</script>
+
+<template>
+  <div class="h-screen">
+    <div class="h-2/6"></div>
+    <div class="columns-3">
+        <CompetitionButton/>
+        <CompetitionButton/>
+        <CompetitionButton/>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
