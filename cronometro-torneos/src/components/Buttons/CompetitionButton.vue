20a210d457c0782ace48e6c0b0da191270c65358
orlandobel <orlando.bel.jimmy@gmail.com>
2023-08-09 22:06:38 -0600

    Home page

--- cronometro-torneos/src/components/Buttons/CompetitionButton.vue
+++ cronometro-torneos/src/components/Buttons/CompetitionButton.vue
@@ -1,21 +1,33 @@
 <script lang="ts" setup>
+const props = defineProps<{
+    image_name: string
+    align: string
+}>()
 
+const image = new URL(`../../assets/images/${props.image_name}.png`, import.meta.url).toString()
+const justify_class = `justify-${props.align}`
 </script>
 
 <template>
-    <div class="h-full flex justify-center items-center">
-        <button>Btn 1</button>
+    <div class="h-full flex" :class="justify_class">
+        <button >
+            <img :src="image" class="h-full">
+        </button>
     </div>
 </template>
 
 <style scoped>
 button {
-    --btn-size: 25vmax;
+    --btn-size: 15vmax;
 
     width: var(--btn-size);
     height: var(--btn-size);
     
     background-color: white;
     border: solid black;
+}
+
+.justify-end {
+    justify-content: flex-end;
 }
 </style>
--- cronometro-torneos/src/views/Home.vue
+++ cronometro-torneos/src/views/Home.vue
@@ -4,11 +4,13 @@ import CompetitionButton from "../components/Buttons/CompetitionButton.vue";
 
 <template>
   <div class="h-screen">
-    <div class="h-2/6"></div>
+    <div class="h-2/5 flex justify-center">
+      <img src="../assets/images/asociacion_zacatecas.png" class="h-full" />
+    </div>
     <div class="columns-3">
-        <CompetitionButton/>
-        <CompetitionButton/>
-        <CompetitionButton/>
+      <CompetitionButton image_name="forma_btn" align="end" />
+      <CompetitionButton image_name="puntos_btn" align="center" />
+      <CompetitionButton image_name="continua_btn" align="start" />
     </div>
   </div>
 </template>
